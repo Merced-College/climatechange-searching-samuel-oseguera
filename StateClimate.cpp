dce9bc179ef780bc20cc383b1d0dfed02a8c7664
sam <samuelojr101@gmail.com>
2025-02-25 18:42:28 +0000

    Updated Main, Added Setters to make code run.

--- StateClimate.cpp
+++ StateClimate.cpp
@@ -10,6 +10,13 @@ int StateClimate::getYear() const { return year; }
 double StateClimate::getTemp() const { return temp; }
 double StateClimate::getTempC() const { return tempc; }
 
+// Setters
+void StateClimate::setFips(int f) { fips = f; }
+void StateClimate::setYear(int y) { year = y; }
+void StateClimate::setTemp(double t) { temp = t; }
+void StateClimate::setTempC(double tc) { tempc = tc; }
+
+
 // Display function
 void StateClimate::display() const {
     std::cout << "FIPS: " << fips << ", Year: " << year 
--- main.cpp
+++ main.cpp
@@ -3,7 +3,54 @@
 #include <fstream>
 #include <vector>
 #include <sstream>
-#include "StateClimate.h"
+//#include "StateClimate.h"
+
+class StateClimate {
+    private:
+        int fips;
+        int year;
+        double temp;
+        double tempc;
+    
+    public:
+        // Constructor
+        StateClimate(int f, int y, double t, double tc);
+    
+        // Getters
+        int getFips() const;
+        int getYear() const;
+        double getTemp() const;
+        double getTempC() const;
+    
+        // Setters
+        void setFips(int f);
+        void setYear(int y);
+        void setTemp(double t);
+        void setTempC(double tc);
+    
+        // Display function
+        void display() const;
+};
+
+StateClimate::StateClimate(int f, int y, double t, double tc) : fips(f), year(y), temp(t), tempc(tc) {}
+
+// Getters
+int StateClimate::getFips() const { return fips; }
+int StateClimate::getYear() const { return year; }
+double StateClimate::getTemp() const { return temp; }
+double StateClimate::getTempC() const { return tempc; }
+
+// Setters
+void StateClimate::setFips(int f) { fips = f; }
+void StateClimate::setYear(int y) { year = y; }
+void StateClimate::setTemp(double t) { temp = t; }
+void StateClimate::setTempC(double tc) { tempc = tc; }
+
+// Display function
+void StateClimate::display() const {
+    std::cout << "FIPS: " << fips << ", Year: " << year 
+              << ", Temp (F): " << temp << ", Temp (C): " << tempc << std::endl;
+}
 
 int main() {
     std::vector<StateClimate> climateData;
